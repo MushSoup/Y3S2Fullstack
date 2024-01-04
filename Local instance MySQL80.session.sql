14ea641a34b4c58ec803507d96476c4831b5c089
MushSoup <112638831+MushSoup@users.noreply.github.com>
2024-01-05 02:15:36 +0800

    modified:   Local instance MySQL80.session.sql 	modified:   config/DBConnection.js 	modified:   index.js 	new file:   models/Attendee.js 	modified:   models/User.js 	modified:   routes/event.js 	modified:   views/event/expandedEvent.handlebars 	modified:   views/event/readEvent.handlebars 	new file:   views/event/registerEvent.handlebars

--- Local instance MySQL80.session.sql
+++ Local instance MySQL80.session.sql
@@ -1,2 +1,2 @@
 
-SELECT * FROM event;
+SHOW CREATE TABLE eventattendee;
--- routes/event.js
+++ routes/event.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const User = require('../models/User');
+const Attendee = require('../models/Attendee');
 const router = express.Router();
 const bodyParser = require("body-parser");
 const moment = require('moment');
 const Event = require('../models/Event');
 const cookieParser = require('cookie-parser');
 const multer = require('multer'); // Import multer
+
 
 router.use(cookieParser());
 
@@ -61,6 +63,91 @@ router.post('/createEvent', upload.single('eventImage'), (req, res) => {
     })
         .catch(err => console.log(err))
 });
+
+router.get('/registerEvent/:eventID', (req, res) => {
+    Event.findOne({
+        where: {
+            eventID: req.params.eventID
+        },
+        raw: true,
+        nest: true
+    }).then((event) => {
+        if (req.user){
+        if (req.user.DOB != null) {
+            const dob = new Date(req.user.DOB);
+            const today = new Date();
+            const caclAge = Math.trunc((today - dob) / (365.25 * 24 * 60 * 60 * 1000));
+            res.render('event/registerEvent', {
+                event: event,
+                aName: req.user.username,
+                aPhoneNo: req.user.tel,
+                aGender: req.user.gender,
+                aAge: caclAge
+            });
+        }else{
+        res.render('event/registerEvent', {
+            event: event,
+            aName: req.user.username,
+            aPhoneNo: req.user.tel,
+            aGender: req.user.gender,
+            aAge:null
+        });
+        }
+    }else{
+        res.render('event/registerEvent', {
+            event: event,
+        })}
+        if (event.eventImg) {
+            event.eventImg = event.eventImg.toString('base64');
+        }
+        
+    }).catch(err => console.log(err));
+});
+
+router.post('/registerEvent/:eventID', (req, res) => {
+    let aName = req.body.aName;
+    let aPhoneNo = req.body.aPhoneNo;
+    let aAge = req.body.aAge;
+    let aGender = req.body.aGender;
+    let aDiet = req.body.aDiet;
+    let aEmerCont = req.body.aEmerCont;
+    let aExtra = req.body.aExtra;
+    let eventID = req.params.eventID;
+    let userID = req.user ? req.user.userID : null;
+    
+if (req.user){
+    Attendee.create({
+        aName,
+        aPhoneNo,
+        aAge,
+        aGender,
+        aDiet,
+        aEmerCont,
+        aExtra,
+        eventID,
+        userID
+    })
+    .then((attendee) => {
+        res.redirect('/event/readEvent');
+    }).catch(err => console.log(err));
+}
+else{
+    Attendee.create({
+        aName,
+        aPhoneNo,
+        aAge,
+        aGender,
+        aDiet,
+        aEmerCont,
+        aExtra,
+        eventID,
+    })
+    .then((attendee) => {
+        res.redirect('/event/readEvent');
+    }).catch(err => console.log(err));
+}
+});
+
 
 router.get('/expandedEvent/:eventID', (req, res) => {
     console.log("In edit, id=",req.params.eventID)
